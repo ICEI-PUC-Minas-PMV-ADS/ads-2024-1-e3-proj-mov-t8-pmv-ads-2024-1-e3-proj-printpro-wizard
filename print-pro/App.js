630a35b6231844be6747fed8e5fbb7a2dcc168ab
splacs <william.ribeiro133@gmail.com>
2024-03-22 17:38:10 -0300

    app.js

--- print-pro/App.js
+++ print-pro/App.js
@@ -1,6 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+import PagRetorno from './src/pages/PagRetorno';
+
+import Footer from "./src/components/Footer"
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import Footer from "./source/components/Footer"
 
 export default function App() {
   return (
