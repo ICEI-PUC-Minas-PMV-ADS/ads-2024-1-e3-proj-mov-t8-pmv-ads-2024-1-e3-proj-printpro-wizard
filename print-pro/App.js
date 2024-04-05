12b65d2b4379f0cd92e15367a0156c32567ec19d
Caique Magalhaes <130615707+Eike94@users.noreply.github.com>
2024-04-05 14:12:29 -0300

    Pagina Resultados

--- print-pro/App.js
+++ print-pro/App.js
@@ -4,11 +4,12 @@ import Footer from './src/pages/Footer/index';
 import PagRetorno from './src/pages/PagRetorno';
 import CalcDetalhado from './src/pages/CalcDetalhado';
 import Orcamento from './src/pages/Orcamento';
+import Resultados from './src/pages/Resultados';
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <CalcDetalhado/>
+      <Resultados/>
     </View>
   );
 }
