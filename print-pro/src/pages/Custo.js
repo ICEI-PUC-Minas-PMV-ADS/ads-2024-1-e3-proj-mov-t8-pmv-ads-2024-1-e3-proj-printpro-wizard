61b600887131c0884a3134b411c855ae0f7d949b
ieeeza <cesardermer1@gmail.com>
2024-05-09 12:29:07 -0300

    custo update

--- print-pro/src/pages/Custo.js
+++ print-pro/src/pages/Custo.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const Custo = () => {
+
+  const [resource, onChangeResource] = React.useState('');
+  const [energy, onChangeEnergy] = React.useState('');
+  const [maintenance, onChangeMaintenance] = React.useState('');
+  const [fails, onChangeFails] = React.useState('');
+  const [finishing, onChangeFinishing] = React.useState('');
+  const [fixation, onChangeFixation] = React.useState('');
+  
   return (
-    <View style={styles.container}> 
-      <Text style={styles.text}>Custo</Text>
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Custo Material</Text>
+        <TextInput keyboardType="numeric" onChangeText={onChangeResource} value={resource} style={styles.input}/>
+
+        <Text style={styles.text}>Custo Energia</Text>
+        <TextInput keyboardType="numeric" onChangeText={onChangeEnergy} value={energy} style={styles.input}/>
+
+        <Text style={styles.text}>Custo Manutenção</Text>
+        <TextInput keyboardType="numeric" onChangeText={onChangeMaintenance} value={maintenance} style={styles.input}/>
+
+        <Text style={styles.text}>Custo de Falhas</Text>
+        <TextInput keyboardType="numeric" onChangeText={onChangeFails} value={fails} style={styles.input}/>
+
+        <Text style={styles.text}>Custo de Acabamento</Text>
+        <TextInput keyboardType="numeric" onChangeText={onChangeFinishing} value={finishing} style={styles.input}/>
+
+        <Text style={styles.text}>Custo de Fixação (spray)</Text>
+        <TextInput keyboardType="numeric" onChangeText={onChangeFixation} value={fixation} style={styles.input}/>
+      </View>
     </View>
 
   );
@@ -12,24 +38,29 @@ const Custo = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
     backgroundColor: 'white'
   },
+
+  textContainer: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    marginTop: 25
+  },
+
   text: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    backgroundColor: '#ADD8E6',
-    padding: 20,
-    width: '100%',
-    color: 'white',
-    outline: 'black',
-    textAlign: 'center',
-    textShadowColor: 'rgba(0,0,0, 0.75)',
-    textShadowOffset: { width: -1, height: 1},
-    textShadowRadius: 5
-  }
+    fontSize: 20,
+  },
+
+  input: {
+    width: '90%',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
 });
 
 export default Custo;
--- print-pro/src/pages/Footer/style.js
+++ print-pro/src/pages/Footer/style.js
@@ -8,7 +8,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingVertical: 5,
         paddingHorizontal: 25,
-        position: 'absolute',
+        position: 'relative',
         bottom: 0,
         left: 0,
         right: 0,
