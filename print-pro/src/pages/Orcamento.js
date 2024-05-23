88377b1f92aa499db4b47f741162fde46b72e5d7
DaviddMarques <125314068+DaviddMarques@users.noreply.github.com>
2024-05-23 19:43:49 -0300

    Update Orcamento.js

--- print-pro/src/pages/Orcamento.js
+++ print-pro/src/pages/Orcamento.js
@@ -7,6 +7,14 @@ const Orcamento = () => {
   return (
     <View style={styles.container}> 
       <Text style={styles.text}>Orcamento</Text>
+      <Text style={styles.time}>Tempo estimado: 9h 28m</Text>
+      <Text style={styles.weight}>Peso estimado: 120g</Text>
+      <View style={{marginTop:70}}>
+        <Text style={styles.sale}>Valor de Venda</Text>
+        <View style={styles.saleView}>
+          <Text style={styles.sale}>R$ 85,53</Text>
+        </View>
+      </View>
       <Footer/>
     </View>
 
@@ -18,7 +26,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+
   },
   text: {
     fontSize: 30,
@@ -32,6 +41,24 @@ const styles = StyleSheet.create({
     textShadowColor: 'rgba(0,0,0, 0.75)',
     textShadowOffset: { width: -1, height: 1},
     textShadowRadius: 5
+  },
+  time: {
+    paddingTop:200,
+    fontSize:25
+  },
+  weight:{
+    fontSize:25
+  },
+  sale:{
+    fontSize:25,
+    textAlign:"center"
+  },
+  saleView:{
+    backgroundColor:'lightgreen',
+    padding:10,
+    paddingLeft:40,
+    paddingRight:40,
+    marginTop:5
   }
 });
 
