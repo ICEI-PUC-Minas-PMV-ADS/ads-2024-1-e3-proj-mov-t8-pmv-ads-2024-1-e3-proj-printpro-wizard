99a83fbd45804b94c164f70ee01452587d52de99
Caique Magalhaes <130615707+Eike94@users.noreply.github.com>
2024-05-24 10:39:55 -0300

    Explicação dos Calculo de Custo

--- print-pro/src/pages/Custo.js
+++ print-pro/src/pages/Custo.js
@@ -10,13 +10,29 @@ export default function Custo() {
   const [fails, setFails] = React.useState();
   const [finishing, setFinishing] = React.useState();
   const [fixation, setFixation] = React.useState();
+
+/* Custo do Material: Valor do material / 1000 * peso estimado(g) 
+exemplo: (150/1000) * 120 = $ 18,00 */
+
+/* Custo de Energia: Definido pelo usuário */
+
+/* Custo de Manutenção: custo do material * 0,15 
+exemplo: 18 * 0,15 = $2,70 */
+
+/* Custo de Falhas: custo de material * media de falhas
+exemplo 18 * 0,1 = $1,8 */
+
+/* Custo do Acabamento (15%): custo de material * 15%
+exemplo 18 * 0,15 = $2,70 */
+
+/* Custo de Fixação (spray): Definido pelo usuário */
   
   return (
     <View style={styles.container}>
 
       <View style={styles.textContainer}>
 
-        <Text style={styles.text}>Custo Material</Text>
+        <Text style={styles.text}>Custo Material</Text> 
         <TextInput keyboardType="numeric" onChangeText={setResource} value={resource} style={styles.input}/>
 
         <Text style={styles.text}>Custo Energia</Text>
